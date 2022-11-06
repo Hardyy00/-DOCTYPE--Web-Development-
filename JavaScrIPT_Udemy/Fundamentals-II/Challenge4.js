b98e5ba8b8778bcf2c16a994d483ce1c4199e7a6
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-11-06 23:19:36 +0530

    Add files via upload

---
+++ JavaScrIPT_Udemy/Fundamentals-II/Challenge4.js
@@ -0,0 +1,33 @@
+`use strict`;
+
+const calcTip = function (bill){
+    
+    return bill>=50 && bill<=300 ? bill*0.15 : bill*0.2;
+}
+
+const calcAvg = function(arr){
+    let sum = 0;
+
+    for(let i=0 ; i<arr.length;i++ ){
+        sum += arr[i];
+    }
+
+    return sum/arr.length;
+}
+
+const bills = [22,295,176,440,37,105,10,1100,86,52];
+
+const tips = [];
+const totals = [];
+
+for(let i=0; i<bills.length;i++){
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(tip + bills[i]);
+}
+
+console.log(`Tips : ${tips}`);
+console.log(`Totals : ${totals}`);
+console.log(`Average of total bill : $${calcAvg(totals)}`);
+
+
